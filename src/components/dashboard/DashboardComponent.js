41223d6e0166c009bcb26bc4b56247be3cfe9cf3
Alejandro Narancio <ale.narancio@gmail.com>
2019-07-24 18:59:00 -0300

    re arranging the dashboard buttons

--- src/components/dashboard/DashboardComponent.js
+++ src/components/dashboard/DashboardComponent.js
@@ -12,10 +12,10 @@ class DashboardComponent extends React.Component {
                 <div class='row'>
                     <div class='column'>
                         <DashboardItemComponent title="My Wallet" icon="fas fa-wallet" />
-                        <DashboardItemComponent title="RNS" icon="fas fa-id-card" />
+                        <DashboardItemComponent title="Lumino" icon="fas fa-bolt" />
                     </div>
                     <div class='column'>
-                        <DashboardItemComponent title="Lumino" icon="fas fa-bolt" />
+                        <DashboardItemComponent title="RNS" icon="fas fa-id-card" />                        
                         <DashboardItemComponent title="Storage" icon="fas fa-database" />
                     </div>
                 </div>
